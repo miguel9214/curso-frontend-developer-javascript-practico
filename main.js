a115db808c15c85af5edb6aa32b27028926f19e7
mguel9214 <iguel921433@gmail.com>
2023-06-22 10:55:11 -0500

    css terminado

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const escritorioMenu = document.querySelector(".desktop-menu");
 const imagenMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const imagenCarrito = document.querySelector(".navbar-shopping-cart");
-const carritoCompras = document.querySelector(".product-detail");
-const cardContenedor =document.querySelector(".cards-container");
+const carritoCompras = document.querySelector("#carritoCompras");
+const cardContenedor = document.querySelector(".cards-container");
 
 imagenMenu.addEventListener("click", cambiarMenuMobile);
 
@@ -63,39 +63,41 @@ productos.push({
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 
-// con esto creo un elemento desde javascritp
-for (producto of productos) {
-  const productCard = document.createElement("div");
-  productCard.classList.add("product-card");
+function mostrarProductos(arr) {
+  // con esto creo un elemento desde javascritp
+  for (producto of productos) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
 
-  const imagenContendero = document.createElement("img");
-  imagenContendero.setAttribute("src", producto.imagen);
+    const imagenContendero = document.createElement("img");
+    imagenContendero.setAttribute("src", producto.imagen);
 
-  const productInfo = document.createElement("div");
-  productCard.appendChild(productInfo);
+    const productInfo = document.createElement("div");
+    productCard.appendChild(productInfo);
 
-  productInfo.classList.add("product-info");
-  const div = document.createElement("div");
+    productInfo.classList.add("product-info");
+    const div = document.createElement("div");
 
+    const parrafoPrecio = document.createElement("p");
+    parrafoPrecio.innerText = "$" + producto.precio;
+    const parrafoNombre = document.createElement("p");
+    parrafoNombre.innerText = producto.nombre;
 
-  const parrafoPrecio = document.createElement("p");
-  parrafoPrecio.innerText = "$" + producto.precio;
-  const parrafoNombre = document.createElement("p");
-  parrafoNombre.innerText = producto.nombre;
-  
-  const figura = document.createElement("figure");
-  const imagenCart = document.createElement("img");
-  imagenCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
-  figura.appendChild(imagenCart);
-  div.appendChild(parrafoPrecio);
-  div.appendChild(parrafoNombre);
-  productCard.appendChild(imagenContendero);
-  productCard.appendChild(productInfo);
-  productInfo.appendChild(div)
-  productInfo.appendChild(figura);
-  cardContenedor.appendChild(productCard);
-
+    const figura = document.createElement("figure");
+    const imagenCart = document.createElement("img");
+    imagenCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    figura.appendChild(imagenCart);
+    div.appendChild(parrafoPrecio);
+    div.appendChild(parrafoNombre);
+    productCard.appendChild(imagenContendero);
+    productCard.appendChild(productInfo);
+    productInfo.appendChild(div);
+    productInfo.appendChild(figura);
+    cardContenedor.appendChild(productCard);
+  }
 }
+
+mostrarProductos(productos);
 
 /*       <div class="product-card">
           <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
